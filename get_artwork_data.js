7fbba5a4ac57aaabdcba2bc867a81529bca0a70a
dearnik1 <nik.khomenko@gmail.com>
2024-10-06 17:28:41 -0500

    formatted json

--- get_artwork_data.js
+++ get_artwork_data.js
@@ -2,7 +2,7 @@ document.getElementById('fetch-button').onclick = () => {
     fetch('https://api.artic.edu/api/v1/artworks/4')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('output').textContent = JSON.stringify(data);
+            document.getElementById('output').textContent = JSON.stringify(data, null, 2);
         })
         .catch(error => {
             document.getElementById('output').textContent = 'Error: ' + error;
